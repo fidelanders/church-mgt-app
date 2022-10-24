7376111ef85f4d0ba006a8409f94aec3e71fb66e
Fidelis Ogbeni <85418769+fidelanders@users.noreply.github.com>
2022-10-24 09:20:09 +0100

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,11 +5,11 @@ const controllers = require("../controllers/userController");
 router.post("/register", controllers.createUser);
 router.post("/verify", controllers.verifyEmail);
 router.post("/login", controllers.login);
-// router.get("/", middleware.authenticate, controllers.getAllUsers);
-// router.get("/:id", middleware.authenticate, controllers.getUserId);
+router.get("/", middleware.authenticate, controllers.getAllUsers);
+router.get("/:id", middleware.authenticate, controllers.getUserId);
 
-router.get("/", controllers.getAllUsers);
-router.get('/:id', controllers.getUserId);
+// router.get("/", controllers.getAllUsers);
+// router.get('/:id', controllers.getUserId);
 
 router.patch("/:id",middleware.authenticate, controllers.updateUser);
 router.delete("/:id",middleware.authenticate, controllers.deleteUser);
